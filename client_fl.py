161e89e2b0a3b36514585175d227a02c6417354d
gbrsouza <gbrsouza20@gmail.com>
2022-10-05 15:19:03 -0300

    add cnn models

--- client_fl.py
+++ client_fl.py
@@ -12,20 +12,23 @@ parser.add_argument("--BATCH_SIZE")
 
 args=parser.parse_args()
 
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"    
+
 def main() -> None:
 
     train_ds = tf.keras.utils.image_dataset_from_directory(
-        f'./datasource/dog-cat/train/client{args.CLIENT_NUMBER}',
+        f'./datasource/client{args.CLIENT_NUMBER}',
         seed=123,
-        validation_split=0.5,
+        validation_split=0.1,
         batch_size=int(args.BATCH_SIZE),
         subset="training"
     )
-    generic_model = GenericModel().getModel()
-    generic_model.build(input_shape=(256, 256, 3))
-    # generic_model = tf.keras.applications.EfficientNetB0(
-    #     input_shape=(256, 256, 3), weights=None, classes=2
-    # )
+    # generic_model = GenericModel().getModel()
+    # generic_model.build(input_shape=(256, 256, 3))
+    generic_model = tf.keras.applications.EfficientNetB0(
+        input_shape=(256, 256, 3), weights=None, classes=2
+    )
     generic_model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
 
     client = FlFrameworkClient(generic_model, train_ds, int(args.EPOCHS))
--- server_fl.py
+++ server_fl.py
@@ -2,13 +2,37 @@ import flwr as fl
 import tensorflow as tf
 from model import GenericModel
 
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+# if gpus:
+#   try:
+#     for gpu in gpus:
+#       tf.config.experimental.set_memory_growth(gpu, True)
+#   except RuntimeError as e:
+#     print(e)
 
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+# if gpus:
+#   try:
+#     tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2048)])
+#   except RuntimeError as e:
+#     print(e)
+
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+# if gpus:
+#   try:
+#     for gpu in gpus:
+#       tf.config.experimental.set_memory_growth(gpu, True)
+#   except RuntimeError as e:
+#     print(e)
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"    
 
 def main() -> None:
 
 
 
-    model = GenericModel().getModel()
+    # model = GenericModel().getModel()
 
     # train_ds = tf.keras.utils.image_dataset_from_directory(
     #     f'./datasource/dog-cat/train/client03',
@@ -19,18 +43,18 @@ def main() -> None:
     # )
     # model.fit(train_ds, epochs=2)
 
-    # model = tf.keras.applications.EfficientNetB0(
-    #     input_shape=(256, 256, 3), weights=None, classes=2
-    # )
-    model.build(input_shape=(256, 256, 3))
+    model = tf.keras.applications.EfficientNetB0(
+        input_shape=(256, 256, 3), weights=None, classes=2
+    )
+    # model.build(input_shape=(256, 256, 3))
     model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
     
     strategy = fl.server.strategy.FedAvg(
         fraction_fit=0.3,
         fraction_evaluate=0.2,
-        min_fit_clients=2,
+        min_fit_clients=1,
         min_evaluate_clients=1,
-        min_available_clients=2,
+        min_available_clients=1,
         evaluate_fn=get_evaluate_fn(model),
         on_fit_config_fn=fit_config,
         on_evaluate_config_fn=evaluate_config,
@@ -47,7 +71,7 @@ def main() -> None:
 def get_evaluate_fn(model):
 
     train_ds = tf.keras.utils.image_dataset_from_directory(
-        f'./datasource/dog-cat/train/client03',
+        f'./datasource/client03',
         validation_split=0.9,
         seed=123,
         batch_size=16,
